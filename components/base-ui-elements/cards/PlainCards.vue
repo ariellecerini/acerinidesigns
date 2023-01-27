0ff1818b7e2b3b280fb3a09e03141d773a860695
ariellecerini <arielle.cerini@createthenext.com>
2023-01-27 16:52:29 -0500

    initial commit

---
+++ components/base-ui-elements/cards/PlainCards.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="section section-plain-cards">
+        
+            <!--     *********    PLAIN BLOG CARDS      *********      -->
+            <client-only>
+              <div class="card-grid row">
+                    <slider-card v-for="post in posts" v-bind:key="post.slug" :prefix="prefix" :post="post" :ct="post.attributes.category"/>
+                   
+                </div>
+            </client-only>
+                
+            </div>
+        </div>
+        <!--     *********    END PLAIN BLOG CARDS      *********      -->
+       
+   
+</template>
+<script>
+    import SliderCard from './SliderCard'; 
+  export default {
+    components: {
+      SliderCard
+    },
+     props: {
+    prefix: String,
+    slug: String,
+    posts: Array, 
+  }
+  }
+</script>
+<style>
+
+.card-grid{
+
+}
+</style>
